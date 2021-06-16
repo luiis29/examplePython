5d079de5cb0aabc49f080cc2e69b7561407f7cf5
luiis29 <51802134+luiis29@users.noreply.github.com>
2021-06-16 17:26:05 -0500

    Se actualizo el esquema de la base de datos.

--- Script DB/python.sql
+++ Script DB/python.sql
@@ -1,40 +1,85 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versión del servidor:         10.5.10-MariaDB - mariadb.org binary distribution
--- SO del servidor:              Win64
--- HeidiSQL Versión:             11.2.0.6213
--- --------------------------------------------------------
+CREATE DATABASE  IF NOT EXISTS `python` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */;
+USE `python`;
+-- MariaDB dump 10.19  Distrib 10.5.10-MariaDB, for Win64 (AMD64)
+--
+-- Host: 127.0.0.1    Database: python
+-- ------------------------------------------------------
+-- Server version	10.5.10-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Table structure for table `permissions`
+--
 
--- Volcando estructura de base de datos para python
-CREATE DATABASE IF NOT EXISTS `python` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci */;
-USE `python`;
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permissions` (
+  `idpermissions` int(11) NOT NULL AUTO_INCREMENT,
+  `iduser` int(11) DEFAULT NULL,
+  `endpoints` text COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `consult_users` int(11) DEFAULT NULL,
+  `add_user` int(11) DEFAULT NULL,
+  `edit_user` int(11) DEFAULT NULL,
+  `delete_user` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idpermissions`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando estructura para tabla python.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` VALUES (19,68,'*',1,1,1,1),(34,158,'*',1,1,1,1),(35,159,'add_user,delete_user,edit_user',1,0,0,1),(36,160,'add_user,delete_user',1,0,1,0);
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `iduser` int(11) NOT NULL AUTO_INCREMENT,
   `names` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
   `lastname` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
-  `role` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
   `user` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB AUTO_INCREMENT=161 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Volcando datos para la tabla python.users: ~1 rows (aproximadamente)
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` (`id`, `names`, `lastname`, `role`, `user`, `password`) VALUES
-	(3, 'Pedro', 'Suarez', NULL, 'Pruebas', '123');
+INSERT INTO `users` VALUES (68,'sdfds','Perez','test','pbkdf2:sha256:260000$GjFnJfoplaQEZpFv$08df74a807ee22f8518bf23061150067a099a20c4cbc557f8f549c4e08a181af'),(158,'Pruebas','2','test1','pbkdf2:sha256:260000$JRr8V9zDrJ802hZD$3484be90f32924efcc90ae31522d8ae14ce35cea4f3c0b5e200ce79303e95142'),(160,'Luis','Espinoza','test5','pbkdf2:sha256:260000$rCkumwItzwaA8mRG$45ad914091a12da7c88a4889d2188ee1aad4f31771f69878f61abc606e06cfe0');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-06-16 17:19:04
